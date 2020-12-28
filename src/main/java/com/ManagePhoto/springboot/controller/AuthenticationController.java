aa60a8a94a840e3e5b2c980494999f05b72c178d
Dang Chau Bao Van <44543505+DangChauBaoVan@users.noreply.github.com>
2020-12-28 10:43:40 +0700

    Update Features
    - Logout
    - Edit Show Photo

--- src/main/java/com/ManagePhoto/springboot/controller/AuthenticationController.java
+++ src/main/java/com/ManagePhoto/springboot/controller/AuthenticationController.java
@@ -3,9 +3,14 @@ package com.ManagePhoto.springboot.controller;
 import java.io.Console;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -85,6 +90,15 @@ public class AuthenticationController {
 		modelAndView.setViewName("addImage"); 
 		return modelAndView;
 	}
+	
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login";
+    }
 	
 	
 }
